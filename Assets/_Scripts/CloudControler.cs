74c5525a6ea11f143c79c26d6b2413ca00d059ef
Jeon-JongChan <loki3773@naver.com>
2018-06-24 19:23:25 +0900

    New Init File
    ignore적용위해서 새롭게 올린 파일들

---
+++ Assets/_Scripts/CloudControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody2D))]
+public class CloudControler : ObjectControl {
+    public GameObject MovePoints;
+    public GameObject[] arr;
+    Rigidbody2D rigidbody;
+  
+
+    void Start () {
+        arr = new GameObject[MovePoints.transform.childCount];
+        for (int i = 0; i < MovePoints.transform.childCount; i++) {
+            arr[i] = MovePoints.transform.GetChild(i).gameObject;
+        }
+        rigidbody = GetComponent<Rigidbody2D>();
+        InitVelocity(rigidbody);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        StartCoroutine(ObjectTargetsTraceMove2D(rigidbody, arr, arr.Length));       
+    }
+
+    
+}
