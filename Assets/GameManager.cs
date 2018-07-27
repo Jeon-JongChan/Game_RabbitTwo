b90f9c94283303540fbaec146158a1ec345503d3
thanks9807 <40519125+thanks9807@users.noreply.github.com>
2018-07-27 20:11:04 +0900

    UI시스템
    Ui만들었다ㅏㅏㅏㅏ

---
+++ Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public GameObject pausePage;
+    bool isPauseing = false;
+    private void Start()
+    {
+        pausePage.SetActive(false);
+    }
+    void Update () {
+        if (Input.GetButton("Cancel")&&!isPauseing) {
+            GamePause();
+        }
+	}
+    public void GamePause()
+    {
+        Time.timeScale = 0;
+        pausePage.SetActive(true);
+        isPauseing = true;
+    }
+    public void GameResume() {
+        Time.timeScale = 1.0f;
+        pausePage.SetActive(false);
+        isPauseing = false;
+    }
+
+    public void GameExit() {
+        print("게임을 종료합니다.");
+        UnityEditor.EditorApplication.isPlaying = false;
+        //Application.Quit();
+    }
+}
