63f5a93585b8b75bb89c80e7aad89f428ec17cb5
thanks9807 <40519125+thanks9807@users.noreply.github.com>
2018-08-11 17:32:46 +0900

    rlaxods

--- Assets/02.Scripts/BreakableWall.cs
+++ Assets/02.Scripts/BreakableWall.cs
@@ -5,9 +5,12 @@ public class BreakableWall : MonoBehaviour {
     [SerializeField] string[] targetTag;
     [Tooltip("벽돌이 사라지고 다시 재생성해야 할 경우 체크")]
     [SerializeField] bool isReActive = false;
+    [Tooltip("벽돌이 다시 사라지고 다시 나타나는데 걸리는 시간")]
+    float reActiveTime = 3f; 
     ParticleSystem particle;
     SpriteRenderer sprite = null;
     BoxCollider2D boxCollider;
+    bool particleSetActive = true;
     private void Start()
     {
         particle = gameObject.GetComponent<ParticleSystem>();
@@ -15,32 +18,49 @@ public class BreakableWall : MonoBehaviour {
         boxCollider = gameObject.GetComponent<BoxCollider2D>();
 
     }
-    void OnEnable() {
+    void Enable() {
         if(isReActive && sprite != null)
         {
             sprite.enabled = true;
             boxCollider.enabled = true;
+            particleSetActive = true;
         }
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(targetTag.Length > 0)
+        if (!particleSetActive)
         {
-            foreach(var v in targetTag)
+            return;
+        }
+        if (targetTag.Length > 0)
+        {
+            
+            foreach (var v in targetTag)
             {
                 if(collision.gameObject.CompareTag(v))
                 {
-                    particle.Play();
-                    sprite.enabled = false;
-                    boxCollider.enabled = false;
+                    
+                     particle.Play();
+                     sprite.enabled = false;
+                     boxCollider.enabled = false;
+                     particleSetActive = false;
+                    if (isReActive) {
+                        Invoke("Enable",reActiveTime);
+                    }  
                 }
             }
         }
-        else{
-            particle.Play();
-            sprite.enabled = false;
-            boxCollider.enabled = false;
-        }
+        //else{
+
+        //     particle.Play();
+        //     sprite.enabled = false;
+        //     boxCollider.enabled = false;
+        //     particleSetActive = false;
+            
+
+        //}
         
     }
+
+    
 }
